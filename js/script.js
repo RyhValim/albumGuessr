46cb7d24c60f275ff3ea96214bcad41395eeaaac
Ryann Gustavo Valim Luiz <ryann.gustavo@cs.up.edu.br>
2024-12-09 11:47:09 -0300

    visual + começo JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 const submitGuess = document.getElementById('submitGuess');
+const guess = document.getElementById('guess')
 
-submitGuess.addEvenetListener('click', (e)=>{
+submitGuess.addEventListener('click', (e)=>{
     e.preventDefault();
+    
+    if(guess.value == 'Trout Mask Replica'){
+        alert('Acertou!');
+    }
+    else{
+        alert('Errou');
+    }
 
 });
